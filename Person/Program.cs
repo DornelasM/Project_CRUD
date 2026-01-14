56b122826bca0804a9b0513fb65f73e851487069
DornelasM <joaopdornelasm@souunit.com>
2026-01-14 01:54:06 -0300

    first commit

---
+++ Person/Program.cs
@@ -0,0 +1,22 @@
+using Person.Data;
+using Person.Routes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<PersonContext>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.PersonRoutes();
+
+app.Run();
---
+++ Person/Routes/PersonRoutes.cs
@@ -0,0 +1,20 @@
+﻿using Person.Data;
+using Person.Models;
+
+namespace Person.Routes;
+
+public static class PersonRoute
+{
+    public static void PersonRoutes(this WebApplication app)
+    {
+        var route = app.MapGroup(prefix: "person");
+        route.MapPost(pattern: "",
+            async (PersonRequest req, PersonContext context) =>
+            {
+                var person = new PersonModel(req.name);
+                await context.AddAsync(person);
+                await context.SaveChangesAsync();
+            });
+    }
+  
+}
